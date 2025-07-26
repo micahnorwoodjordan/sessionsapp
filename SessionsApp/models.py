be8fe27887853402f00be22c3c5c68dbbbcac15e
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-07-26 13:38:01 -0700

    add user modeladmin + define user model + UserManager overrides

--- SessionsApp/models.py
+++ SessionsApp/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, password: str | None = None, **extra_fields) -> 'User':
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email: str, password: str, **extra_fields) -> 'User':
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields['is_staff'] or not extra_fields['is_superuser']:
+            raise ValueError("Superuser must have is_staff=True and is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Access to Django Admin
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS: list[str] = []
+
+    def __str__(self):
+        return self.email
